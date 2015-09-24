1ed4ed427ec0de3d39e68ca0a8521cccbee53544
Ben <narujo@freenet.de>
2015-09-24 10:28:57 +0200

    Changed guest_path back to main_path

--- app/guest/controllers/main_controller.rb
+++ app/guest/controllers/main_controller.rb
@@ -14,7 +14,7 @@ module Guest
     # the main template contains a #template binding that shows another
     # template.  This is the path to that template.  It may change based
     # on the params._controller and params._action values.
-    def guest_path
+    def main_path
       "#{params._component || 'main'}/#{params._controller || 'main'}/#{params._action || 'index'}"
     end
   end
