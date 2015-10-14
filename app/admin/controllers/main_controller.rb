13b5cfa8253c2770aac5c7ed7254c98c5a0c7317
Ben <narujo@freenet.de>
2015-10-14 14:06:46 +0200

    Now playing track addet. Fix add Track to Party.

--- app/admin/controllers/main_controller.rb
+++ app/admin/controllers/main_controller.rb
@@ -23,9 +23,7 @@ module Admin
       store._parties.first._tracks.all.size.then do |tracks_cou|
         if tracks_cou < 11
           user_id = get_random_user_id
-          puts user_id.value
-          track_id = get_random_track_id(user_id.value)
-          puts track_id
+          get_random_track_and_add(user_id)
         end
       end
     end
@@ -44,22 +42,41 @@ module Admin
             stop = false if i > 0
         end
       end
-      return users.value[r]._id
+      return users.value[r]._id.value
     end
 
-    def get_random_track(user)
-      tracks = user.tracks.all
-      count = 0
-      tracks.count.then{|i| count = i-1}
-      r = 0
-      if count > 0
-        r = (0..count).to_a.sample
+    def get_random_track_and_add(user_id)
+      page._party_guests.value.where(id: user_id).first.then do |user|
+        user.tracks.all.then do |tracks|
+          r = (0..(tracks.length.value-1)).to_a.sample
+          i = 0
+          tracks.each do |track|
+            if i == r
+              store
+                .tracks
+                .create(spotifyID: track.spotifyID,name: track.name,length: track.length,artist: track.artist,album: track.album,imgUrl: track.imgUrl,url: track.url)
+                .then do |res|
+                  store.parties.first.tracks.append(res)
+                end.fail{ flash._errors << "Da ist irgendet was schiefgegangen :/" }
+              Volt.logger.info(track.name)
+            end
+            i += 1
+          end
+        end
       end
-      tracks.value[r].value
     end
 
     def next_track
-
+      store.parties.first.tracks.first.destroy
+      store.parties.first.tracks.first.then do |track|
+        Volt.logger.info('next_track')
+        page._track = track.url
+        page._track_name = track.name
+        page._track_album = track.album
+        page._track_artist = track.artist
+        page._track_image = track.imgUrl
+      end
+      `$(#{first_element}).find('#player').load();`
     end
 
     def newParty
@@ -79,6 +96,20 @@ module Admin
       `$(#{first_element}).find('.player').on('ended', function() {`
           next_track
         `});`
+
+        if page._track == nil
+          store.parties.first.tracks.first.then do |track|
+            page._track = track.url
+            page._track_name = track.name
+            page._track_album = track.album
+            page._track_artist = track.artist
+            page._track_image = track.imgUrl
+          end
+          `$(#{first_element}).find('#player').load();`
+        else
+          `$(#{first_element}).find('#player').load();`
+        end
+        loadPlayer
     end
 
     private
